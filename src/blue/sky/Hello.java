589cfeca86afc5567724e57b8b1d71aab4f05f3a
gmaroo2 <greenmaroo@gmail.com>
2015-01-11 21:19:08 -0500

    modify Hello.java 2

--- src/blue/sky/Hello.java
+++ src/blue/sky/Hello.java
@@ -4,7 +4,7 @@ import blue.sky.HelloUtil;
 public class Hello {
 	
     public static void main(String []args) {
-        System.out.println("Hello World  - try Git !!");
+        System.out.println("Hello World  - try Git ver2 !!");
         
         HelloUtil.printMsg("Success !! Good-Bye !");
      }
