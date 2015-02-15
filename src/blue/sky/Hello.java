1168c8b878874842dab7798cb08ff69c9fc8f098
greenmaroo <greenmaroo@gmail.com>
2015-02-15 14:53:24 -0500

    Fix semi-colon

--- src/blue/sky/Hello.java
+++ src/blue/sky/Hello.java
@@ -4,7 +4,7 @@ import blue.sky.HelloUtil;
 public class Hello {
 	
     public static void main(String []args) {
-        System.out.println("Hello World  - create error !!")
+        System.out.println("Hello World  - fix error !!");
         
         HelloUtil.printMsg("Success !! Good-Bye !");
      }
