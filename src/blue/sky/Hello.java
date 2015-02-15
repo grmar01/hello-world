ded20e1271b23550b446ad7328538ae0dc6e4e89
greenmaroo <greenmaroo@gmail.com>
2015-02-15 15:02:24 -0500

    add #1
    add printMsg add #1

--- src/blue/sky/Hello.java
+++ src/blue/sky/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	
     public static void main(String []args) {
         System.out.println("Hello World  - fix error !!");
+
+        HelloUtil.printMsg("Add #1");
         
         HelloUtil.printMsg("Success !! Good-Bye !");
      }
