574439284497bda673211c91a641b355aa615009
gmaroo_s1 <gmaroo_s1@localhost>
2015-03-12 00:33:20 -0400

    change Hello.java

--- src/blue/sky/Hello.java
+++ src/blue/sky/Hello.java
@@ -4,11 +4,7 @@ import blue.sky.HelloUtil;
 public class Hello {
 	
     public static void main(String []args) {
-        System.out.println("Hello World from ubuntu Go!! ");
-
-        HelloUtil.printMsg("Add #1");
-        HelloUtil.printMsg("Add #1 from gitbash");
-        HelloUtil.printMsg("Add #2 from gitgui");
+        System.out.println("Hello World from ubuntu Go Go Go!! ");
         
         HelloUtil.printMsg("Success !! Good-Bye !");
      }
