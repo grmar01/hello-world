55a3aefeb8bbef21d0a35e203f7ead6943c25478
Chadol <chadol@hp1-ubuntu.org>
2015-03-15 17:24:14 -0400

    add msg with wrong issue #6 in Hello.java

--- src/blue/sky/Hello.java
+++ src/blue/sky/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
     public static void main(String []args) {
         System.out.println("Hello World from chadol on hp1-ubuntu !! ");
         HelloUtil.printMsg("*** 1st change in HelloUtil ***");
+        HelloUtil.printMsg("*** put wrong issue# - 1st change #6 in HelloUtil ***");
         HelloUtil.printMsg("Good-Bye from chadol !!");
      }
 
