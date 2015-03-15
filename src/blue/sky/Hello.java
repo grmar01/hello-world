8add4c5a632ee58fda2864782061fc590a171d00
Chadol <chadol@hp1-ubuntu.org>
2015-03-15 18:19:20 -0400

    No.2a add msg with wrong issue numbers #6 #7 #8 in Hello.java

--- src/blue/sky/Hello.java
+++ src/blue/sky/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
     public static void main(String []args) {
         System.out.println("Hello World from chadol on hp1-ubuntu !! ");
         HelloUtil.printMsg("*** 1st change in HelloUtil ***");
-        HelloUtil.printMsg("put wrong issue# - 1st change #6 in HelloUtil ***");
+        HelloUtil.printMsg("one more - put wrong issue# - 1st change #5 in HelloUtil ***");
         HelloUtil.printMsg("put correct issue# - 1st change #7 in HelloUtil ***");
         HelloUtil.printMsg("put wrong issue# - 1st change #8 in HelloUtil ***");
         HelloUtil.printMsg("Good-Bye from chadol !!");
