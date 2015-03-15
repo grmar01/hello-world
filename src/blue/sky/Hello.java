56e8b3743b87774ec35547d7b4e56c5a9e085d22
Chadol <chadol@hp1-ubuntu.org>
2015-03-15 17:18:44 -0400

    change msg in Hello.java

--- src/blue/sky/Hello.java
+++ src/blue/sky/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	
     public static void main(String []args) {
         System.out.println("Hello World from chadol on hp1-ubuntu !! ");
-        HelloUtil.printMsg("*** 1 ***");
+        HelloUtil.printMsg("*** 1st change in HelloUtil ***");
         HelloUtil.printMsg("Good-Bye from chadol !!");
      }
 
