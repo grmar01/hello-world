3a0a569dc81c17ace9665e5a54f9c7d9baf479cb
Green M <greenmaroo@gmail.com>
2015-03-31 21:17:30 -0400

    Update Hello.java

--- src/blue/sky/Hello.java
+++ src/blue/sky/Hello.java
@@ -4,7 +4,7 @@ import blue.sky.HelloUtil;
 public class Hello {
 	
     public static void main(String []args) {
-        System.out.println("Hello World from chadol on hp1-ubuntu !! ");
+        System.out.println("** Test Run ** Hello World from chadol on hp1-ubuntu !! ");
         HelloUtil.printMsg("*** 1st change in HelloUtil ***");
         HelloUtil.printMsg("one more - put wrong issue# - 1st change #5 in HelloUtil ***");
         HelloUtil.printMsg("put correct issue# - 1st change #7 in HelloUtil ***");
